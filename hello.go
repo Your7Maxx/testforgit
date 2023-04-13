3ce627c00ed18760c203fe921cc8d4d6d63bf456
P1nH4ck <2586817558@qq.com>
2023-04-13 13:00:22 +0000

    go

---
+++ hello.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "net/url"
+)
+
+func main() {
+    urlValues := url.Values{}
+    urlValues.Add("name","zhaofan")
+    urlValues.Add("age","22")
+    resp, _ := http.PostForm("https://www.baidu.com",urlValues)
+    body, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println(string(body))
+}
